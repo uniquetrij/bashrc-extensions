628e826f58ef623855bb1fb4d2b7711389383101
Trijeet Modak <uniquetrij@gmail.com>
2025-05-31 23:44:44 +0530

    Update extrc-installer.sh

--- extrc-installer.sh
+++ extrc-installer.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-curl -s https://raw.githubusercontent.com/uniquetrij/bashrc-extensions/refs/heads/main/extrc > ~/.extrc
+wget -O - https://raw.githubusercontent.com/uniquetrij/bashrc-extensions/refs/heads/main/extrc > ~/.extrc
 grep -q "source ~/.extrc" ~/.bashrc || echo "source ~/.extrc" >> ~/.bashrc
 bash -c "source ~/.bashrc"
